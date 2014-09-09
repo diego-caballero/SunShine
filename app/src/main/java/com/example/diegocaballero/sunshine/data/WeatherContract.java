99cc6a98341ab90b8f28036569f03eec1b6b9ae6
dcaballero <dcaballero@altimetrik.com>
2014-09-09 18:13:22 -0300

    Uri matcher with locations

--- app/src/main/java/com/example/diegocaballero/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/example/diegocaballero/sunshine/data/WeatherContract.java
@@ -15,15 +15,23 @@
  */
 package com.example.diegocaballero.sunshine.data;
 
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
+
 
 /**
  * Defines table and column names for the weather database.
  */
 public class WeatherContract {
+
+    public static final String CONTENT_AUTHORITY = "com.example.diegocaballero.sunshine";
+    public static final Uri BASE_CONTENT_URL = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String WEATHER_PATH = "weather";
+    public static final String LOCATION_PATH = "location";
     /* Inner class that defines the table contents of the weather table */
     public static final class WeatherEntry implements BaseColumns {
-
+        public static final Uri CONTENT_URI = BASE_CONTENT_URL.buildUpon().appendPath(WEATHER_PATH).build();
         public static final String TABLE_NAME = "weather";
 
         // Column with the foreign key into the location table.
@@ -52,10 +60,43 @@ public class WeatherContract {
 
         // Degrees are meteorological degrees (e.g, 0 is north, 180 is south).  Stored as floats.
         public static final String COLUMN_DEGREES = "degrees";
+
+        public static Uri buildWeatherUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buildWeatherLocation(String locationSetting) {
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).build();
+        }
+
+        public static Uri buildWeatherLocationWithStartDate(
+                String locationSetting, String startDate) {
+            return CONTENT_URI.buildUpon().appendPath(locationSetting)
+                    .appendQueryParameter(COLUMN_DATETEXT, startDate).build();
+        }
+
+        public static Uri buildWeatherLocationWithDate(String locationSetting, String date) {
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).appendPath(date).build();
+        }
+
+        public static String getLocationSettingFromUri(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
+
+        public static String getDateFromUri(Uri uri) {
+            return uri.getPathSegments().get(2);
+        }
+
+        public static String getStartDateFromUri(Uri uri) {
+            return uri.getQueryParameter(COLUMN_DATETEXT);
+        }
     }
 
     public static final class LocationEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URL.buildUpon().appendPath(LOCATION_PATH).build();
 
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + LOCATION_PATH;
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + LOCATION_PATH;
         // Table name
         public static final String TABLE_NAME = "location";
 
@@ -71,6 +112,10 @@ public class WeatherContract {
         // map intent, we store the latitude and longitude as returned by openweathermap.
         public static final String COLUMN_COORD_LAT = "coord_lat";
         public static final String COLUMN_COORD_LONG = "coord_long";
+
+        public static Uri getLocationById(int id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/diegocaballero/sunshine/data/WeatherProvider.java
@@ -0,0 +1,62 @@
+package com.example.diegocaballero.sunshine.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+
+/**
+ * Created by admin on 9/9/14.
+ */
+public class WeatherProvider extends ContentProvider {
+    private static final int WEATHER = 100;
+    private static final int WEATHER_WITH_LOCATION = 101;
+    private static final int WEATHER_WITH_LOCATION_AND_DATE = 102;
+    private static final int LOCATION = 300;
+    private static final int LOCATION_ID = 301;
+
+    public static UriMatcher matcher = buildUriMatcher();
+
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = WeatherContract.CONTENT_AUTHORITY;
+        matcher.addURI(authority, WeatherContract.WEATHER_PATH, WEATHER);
+        matcher.addURI(authority, WeatherContract.WEATHER_PATH + "/*", WEATHER_WITH_LOCATION);
+        matcher.addURI(authority, WeatherContract.WEATHER_PATH + "/*" + "/*", WEATHER_WITH_LOCATION_AND_DATE);
+        matcher.addURI(authority, WeatherContract.LOCATION_PATH , LOCATION);
+        matcher.addURI(authority, WeatherContract.LOCATION_PATH + "/#", LOCATION_ID);
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings2, String s2) {
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
+}
